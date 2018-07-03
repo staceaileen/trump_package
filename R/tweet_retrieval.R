c1073fbb9bf417d3c387e9afa83e936d5f19f2a6
Stacy K <stacykam@mail.usf.edu>
2018-07-03 12:31:08 +0200

    added date input

--- R/tweet_retrieval.R
+++ R/tweet_retrieval.R
@@ -10,7 +10,7 @@ library(rlist)
 #'
 #' @return Data frame of relevant information regarding tweets about Donald Trump
 #' @export
-tweets <- function(filter_word = "") {
+tweets <- function(inputdate, filter_word = "") {
   # configuration
   consumer_key <- "ul0zL1WmoQ9tKWgv0f5AZhiDn"
   consumer_secret <- "TJ3i0v6wNFx91nqfNNbVFSYyDC9vXp1QuOYo0gZ8HdiSTnDmcF"
@@ -21,7 +21,9 @@ tweets <- function(filter_word = "") {
   # getting tweets using twitteR API
   twitteR::setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-  tw = twitteR::searchTwitter(paste0("Trump ", "+ ", filter_word), n = 2500, lang = 'en', retryOnRateLimit = 1e3)
+  tw = twitteR::searchTwitter(paste0("Trump ", "+ ", filter_word), n = 2500,
+                              since = as.character(inputdate - 1), until = as.character(inputdate),
+                              lang = 'en', retryOnRateLimit = 1e3)
   d = twitteR::twListToDF(tw)
   d
 }
