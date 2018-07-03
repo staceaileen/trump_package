0bf0c11d42b8b71f6e6eac32fc3218b2118d1637
Stacy K <stacykam@mail.usf.edu>
2018-07-03 14:45:26 +0200

    added input value to func

--- R/lukedatacleaning.R
+++ R/lukedatacleaning.R
@@ -9,7 +9,7 @@ library(RCurl)
 #'
 #' @return Tidy data of tweets broken down by word and tweet index
 #' @export
-clean_data <- function() {
+clean_data <- function(d) {
   # configuration
   tweet_created = d %>% select(text, created)
 
